95290c00fbe882d641f1d9576b508dd68499fee1
Tejassonar <sonartejas908@gmail.com>
2021-10-02 20:54:35 +0530

    Contains main method

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Day14assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region LinkedList
+            //LinkedList List = new LinkedList();
+            //List.Add(70);
+            //List.Add(30);
+            //List.Add(56);
+            //Console.WriteLine("List created :");
+            //List.Display();
+            //#endregion
+
+
+            //#region LinkedList
+            //LinkedList List = new LinkedList();
+            //List.Add(70);
+            ////List.Add(30);
+            //List.Add(56);
+            //Console.WriteLine("List created :");
+            //List.Display();
+            //List.InsertParticularPosition1(2, 30);
+            //List.Display();
+            //#endregion
+
+            //#region LinkedList
+            //LinkedList List = new LinkedList();
+            //List.Add(70);
+            //List.Add(30);
+            //List.Add(56);
+            //Console.WriteLine("List created :");
+            //List.Display();
+            //List.Pop();
+            //Console.WriteLine("After deleting first elament :");
+            //List.Display();
+            //#endregion
+
+            //#region LinkedList
+            //LinkedList List = new LinkedList();
+            //List.Add(70);
+            //List.Add(30);
+            //List.Add(56);
+            //Console.WriteLine("List created :");
+            //List.Display();
+            //List.PopLast();
+            //Console.WriteLine("After deleting last element :");
+            //List.Display();
+            //#endregion
+
+            //#region LinkedList
+            //LinkedList List = new LinkedList();
+            //List.Add(70);
+            //List.Add(30);
+            //List.Add(56);
+            //Console.WriteLine("List created :");
+            //List.Display();
+            //Console.WriteLine("searching for key 30");
+            //List.Search(30);
+            //List.Display();
+            //#endregion
+
+            //#region LinkedList
+            //LinkedList List = new LinkedList();
+            //List.Add(56);
+            //List.Add(30);
+            //List.Add(70);
+            //List.Display();
+            //Console.WriteLine("List created :");
+            //List.InsertAfter(30, 41);
+            //#endregion
+
+        }
+    }
+}
